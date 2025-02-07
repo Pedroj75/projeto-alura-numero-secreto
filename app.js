f8cf00e1564a669f49832f513f0f8ac07d7bd090
Pedro Alcantara <pedroj.75@outlook.com>
2025-02-07 19:50:44 -0300

    Primeiro Comit

---
+++ app.js
@@ -0,0 +1,29 @@
+alert('Jogo do numero secreto');
+let numeroMaximo = 10
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let tentativa;
+let palpites = 1;
+
+while (tentativa != numeroSecreto) {
+    tentativa = prompt(`Escolha um numero entre 1 e ${numeroMaximo}`);
+    // tentativa for igual numero secreto
+    if (numeroSecreto == tentativa) {
+        break;
+    } else {
+        if (numeroSecreto > tentativa) {
+            alert(`o numero secreto é maior que ${tentativa}`);
+        } else {
+            alert(`o numero secreto e menor que ${tentativa}`);
+        }
+        // palpites = palpites + 1;
+        palpites++
+    }
+}
+
+let palavraPalpite = palpites > 1 ? 'palpites' : 'palpite';
+alert(`Boa, você acertou o numero secreto ${numeroSecreto} com ${palpites} ${palavraPalpite}`);
+//if (palpites > 1) {
+//  alert(`Boa, você acertou o numero secreto ${numeroSecreto} com ${palpites} palpites`);
+//} else {
+//    alert(`Boa, você acertou o numero secreto ${numeroSecreto} com ${palpites} palpite`);
+//}
